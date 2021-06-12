d0d97aad46097a2ab71113545dd4910aa3dc6218
João Lenon <lenonsec7@gmail.com>
2021-06-12 14:18:01 -0300

    feat: Export file and directory contract

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+export * from './src/FileContract'
+export * from './src/DirectoryContract'
 export * from './src/PaginationContract'
 export * from './src/RepositoryContract'
 export * from './src/ApiRequestContract'
